33cdacc27fa5afe28d1796d164f87fa7730fdb65
shrutishibu <sshrutishibu@gmail.com>
2023-10-06 13:59:32 +0530

    update views and urls

--- api_3/urls.py
+++ api_3/urls.py
@@ -6,3 +6,4 @@ urlpatterns = [
     path('upload-image/', views.ImageUploadAndList.as_view(), name='upload_image'),
     #path('image-data/', views.ImageDataList.as_view(), name='image_data_list'),
 ]
+ 
